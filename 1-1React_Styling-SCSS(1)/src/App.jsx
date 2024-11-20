4d6b2af76a563c16213520e02418e46a295e2da8
MyangSZ <unoyou@naver.com>
2024-11-20 22:56:15 +0900

    React심화 챕터1-3 REACT 스타일링(2)

---
+++ 1-1React_Styling-SCSS(1)/src/App.jsx
@@ -0,0 +1,27 @@
+import { useState } from 'react'
+import './App.scss'
+
+function App() {
+  
+
+  return (
+    <>
+    <div className="test">테스트용 div</div>
+    <h2>SCSS 변수 연습</h2>
+    <ul>
+      <li>하이</li>
+      <li>Hi</li>
+      <li>Hello</li>
+      <li>Welcome</li>
+    </ul>
+    <section>
+      <article>SCSS문법에는</article>
+      <article>변수 사용과</article>
+      <article>중첩 문법과</article>
+      <article>믹스인 등이 있습니다.</article>
+    </section>
+    </>
+  )
+}
+
+export default App
