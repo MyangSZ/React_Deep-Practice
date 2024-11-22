ca316adf39eaeb40f57b7fa4989a3d4046224963
MyangSZ <unoyou@naver.com>
2024-11-22 18:23:03 +0900

    React심화 챕터 1-7 React_TailwindCSS(1)

---
+++ 1-7React_TailwindCSS(1)/src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  
+  return (
+    <>
+    <div className="flex justify-center items-center h-screen">
+      <div className="text-8xl text-[#ff69b4]">안녕하세요</div>      
+    </div>
+    </>
+  )
+}
+
+export default App
