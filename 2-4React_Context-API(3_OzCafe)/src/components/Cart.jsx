f77a97099c06bd49476e96e46e1e21a817b155de
MyangSZ <unoyou@naver.com>
2024-11-26 15:28:24 +0900

    React심화 챕터2-4React_Context-API(3)Oz_cafe Porps-Drilling -> ContextAPI로 변경 context폴더추가 cartContext.jsx 파일 추가 menuContext.jsx 파일 추가가

---
+++ 2-4React_Context-API(3_OzCafe)/src/components/Cart.jsx
@@ -0,0 +1,68 @@
+import data from "../assets/data";
+import { useCart } from "../context/cartContext";
+import { useMenu } from "../context/menuContext";
+
+function Cart({}) {
+  const { menu } = useMenu();
+  const { cart } = useCart();
+
+  if (!menu)
+    return (
+      <div style={{ textAlign: "center", margin: "80px" }}>
+        {" "}
+        메뉴 정보가 없어요!
+      </div>
+    );
+  const allMenus = [...menu.커피, ...menu.논커피];
+  return (
+    <>
+      <h2>장바구니</h2>
+      <ul className="cart">
+        {cart?.length ? (
+          cart.map((el) => (
+            <CartItem
+              key={el.id}
+              item={allMenus.find((menu) => menu.id === el.id)}
+              options={el.options}
+              quantity={el.quantity}
+            />
+          ))
+        ) : (
+          <div className="no-item">장바구니에 담긴 상품이 없어요!</div>
+        )}
+      </ul>
+    </>
+  );
+}
+
+function CartItem({ item, options, quantity }) {
+  const { cart, setCart, removeFromCart } = useCart();
+  return (
+    <li className="cart-item">
+      <div className="cart-item-info">
+        <img height={100} src={item.img} />
+        <div>{item.name}</div>
+      </div>
+      <div className="cart-item-option">
+        {Object.keys(options).map((el) => (
+          <div>
+            {el} : {data.options[el][options[el]]}
+          </div>
+        ))}
+        <div>개수 : {quantity}</div>
+      </div>
+      <button
+        className="cart-item-delete"
+        onClick={() => {
+          // 삭제버튼 클릭시 아이템ID 일치여부 확인 후 일치하는 메뉴 제외 후 나머지만 남겨준다.
+          // 일부만 남기는 필터메서드 사용
+          // 요소 ID가 삭제하기 버튼 누를때 일치하는지 확인 후 일치하지 않는 요소들만 화면에 남긴다.
+          removeFromCart(item.id);
+        }}
+      >
+        삭제
+      </button>
+    </li>
+  );
+}
+export default Cart;
---
+++ 2-4React_Context-API(3_OzCafe)/src/components/Menu.jsx
@@ -0,0 +1,54 @@
+import { useState } from "react";
+import Item from "./Item";
+import OrderModal from "./OrderModal";
+import { useMenu } from "../context/menuContext";
+
+// 메뉴를 받아오는 코드
+function Menu() {
+  const { menu } = useMenu();
+
+  if (!menu)
+    return (
+      <div style={{ textAlign: "center", margin: "80px" }}>
+        {" "}
+        메뉴 정보가 없어요!
+      </div>
+    );
+
+  const [modalOn, setModalOn] = useState(false);
+  const [modalMenu, setModalMenu] = useState(null);
+  const categorys = Object.keys(menu);
+  return (
+    <>
+      {categorys.map((category) => {
+        return (
+          <section key={category}>
+            <h2>{category}</h2>
+            <ul className="menu">
+              {/* 받아온 메뉴 상태를 map메서드를 사용해 아이템을 뿌려서 렌더링 */}
+              {menu[category].map((item) => (
+                <Item
+                  key={item.name}
+                  item={item}
+                  clickHandler={() => {
+                    // setModalMenu - 초기값은 null. 아이템을 렌더링 할때 사용하고 있는 아이템 값.
+                    // 하나하나의 정보를 모달메뉴 상태로 넣어준다.
+                    // OrderModal이 받아서 클릭 시 클릭 상품을 모달안에 표시하기 위해 사용된다.
+                    setModalMenu(item);
+                    // setModalOn - OrderModal을 띄울지 말지 상태 관리 변경. 메뉴 모달을 띄울지 말지 상태관리
+                    setModalOn(true);
+                  }}
+                />
+              ))}
+            </ul>
+          </section>
+        );
+      })}
+      {modalOn ? (
+        <OrderModal modalMenu={modalMenu} setModalOn={setModalOn} />
+      ) : null}
+    </>
+  );
+}
+
+export default Menu;
