e75248c3867ce0ddf4175fa91d79c1bcee3682a2
MyangSZ <unoyou@naver.com>
2024-11-29 13:37:20 +0900

    3-2React_Potimisztion-function_TodoList
    useMemo 사용하기-  호출 함수 결과 저장
    useCallback 사용하기-  함수 저장(함수자체 저장)
    memo 사용하기-  리렌더링 방지효과 / 컴포넌트 저장

---
+++ 3-3React_Lazy&Suspense/src/App.jsx
@@ -0,0 +1,45 @@
+import { lazy, Suspense, useState } from "react";
+import "./App.css";
+import { Route, Routes, useNavigate } from "react-router-dom";
+// import Main from "./page/Main";
+// import Detail from "./page/Detail";
+// import Search from "./page/Search";
+
+// lazy 사용하기
+// 인자로 함수 전달. import 함수가 페이지가 있는 경로 전달받는 형태
+const Main = lazy(() => import("./page/Main"));
+// 디테일 페이지
+const Detail = lazy(() => import("./page/Detail"));
+// 서치 페이지
+const Search = lazy(() => import("./page/Search"));
+
+function App() {
+  const [inputValue, setInputValue] = useState("");
+  const navigate = useNavigate();
+
+  return (
+    <>
+      <header>
+        <h1>💚 동물 조아 💚</h1>
+        <input
+          value={inputValue}
+          onChange={(event) => setInputValue(event.target.value)}
+        />
+        <button onClick={() => navigate(`/search?animal=${inputValue}`)}>
+          검색
+        </button>
+      </header>
+      {/* Suspense사용하기 */}
+      <Suspense fallback={<h1>로딩중...</h1>}>
+        <Routes>
+          <Route path="/" element={<Main />}></Route>
+          <Route path="/detail/:id" element={<Detail />}></Route>
+          <Route path="/search" element={<Search />}></Route>
+        </Routes>
+      </Suspense>
+      <footer>all rights reserved to OZ</footer>
+    </>
+  );
+}
+
+export default App;
